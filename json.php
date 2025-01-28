c5dd8baf4dc7629d9c2c1104d850541a20385f59
Joshua Carroll <JoshuaCarroll@users.noreply.github.com>
2025-01-28 15:19:16 -0600

    Removed PHP files.  Created initial dotnet app.

--- json.php
+++
@@ -1,5 +0,0 @@
-<?php 
-        require "repeaters.class.php";
-        $repeaters = new Repeaters();
-        echo '{ "repeaters": ' . json_encode($repeaters) . '}';
-?>
--- warning.sh
+++
@@ -1,1 +0,0 @@
-php  /var/www/Site1/htdocs/RepeaterWarning/index.php > /dev/null 2>&1
